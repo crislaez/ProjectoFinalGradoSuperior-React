de2cfb6642c06143287c057998434d454f33e5b4
crislaez <crislaez30@gmail.com>
2020-03-18 21:05:18 +0100

    'actualizada'

--- src/index.js
+++ src/index.js
@@ -4,13 +4,7 @@ import App from './components/App';
 import firebase from 'firebase';
 
 firebase.initializeApp({
-    apiKey: "AIzaSyCmYu5R72-PUipeu6Of2alQQyNH80CpJ8Y",
-    authDomain: "retofinal-60fda.firebaseapp.com",
-    databaseURL: "https://retofinal-60fda.firebaseio.com",
-    projectId: "retofinal-60fda",
-    storageBucket: "retofinal-60fda.appspot.com",
-    messagingSenderId: "570136171188",
-    appId: "1:570136171188:web:9ed4918bc084b94b273de1"
+
 })
 // import * as serviceWorker from './serviceWorker';
 let root = document.querySelector('#root')
