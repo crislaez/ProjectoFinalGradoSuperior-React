60b33f943ba253e4c2fde4bae223a32e73cd1b88
CrisLaez <you@example.comcrislaez30@gmail.com>
2020-05-15 14:43:49 +0200

    media queries

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ class Footer extends React.Component{
         return(
             <footer>
                 <div className='divFooter'>
-                    <a style={{marginLeft:'20%'}} href='https://es-es.facebook.com/'><span style={{color:'#579EEB'}}><FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon></span> Facebook</a>
+                    <a className='primerEnlace'  href='https://es-es.facebook.com/'><span style={{color:'#579EEB'}}><FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon></span> Facebook</a>
                     <a href='https://www.instagram.com/?hl=es'><span style={{color:'#F95B6E'}}><FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon></span> Instagram</a>
                     <a href='https://twitter.com/'><span style={{color:'#579EEB'}}><FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon></span> Twitter</a>
                     <a href='https://www.twitch.tv/'><span style={{color:'#503EA9'}}><FontAwesomeIcon icon={faTwitch}></FontAwesomeIcon></span> Twitch</a>
